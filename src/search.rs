2bc6b5b8441d487c16eb8e1afa0748018f1f28e1
Koki Honda <kokihonda@am.ics.keio.ac.jp>
2020-11-23 10:50:38 +0900

    add struct in search.rs

--- src/search.rs
+++ src/search.rs
@@ -116,3 +116,22 @@ pub fn bfs01(
         return Some(ans);
     }
 }
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State {
+    cost: usize,
+    pos: (usize, usize),
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+// `PartialOrd` needs to be implemented as well.
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
